3ce9bccc79e7f362df3170809b32f8be460c3f72
Raff <raffaele.schirripa@gmail.com>
2020-01-13 10:44:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,18 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = var.location
 }
+
+module "web_app_container" {
+   source = "app.terraform.io/raff_tfe_workshop/web-app-container/azurerm"
+   version = "2.2.1" 
+   name = "${var.prefix}-app"
+   port = "80"
+   https_only = "false"
+   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+   container_type = "docker"
+   container_image = "scarolan/palacearcade"
+}
+output "container_app_url" {
+   value = "http://${module.web_app_container.hostname}"
+}
+
